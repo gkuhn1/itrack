57bc26e6e210bea01b6dbb1217b39105954d3096
Guilherme Kuhn <g.kuhn0@gmail.com>
2016-01-20 16:55:58 -0200

    Structure and rspec installation

---
+++ app/controllers/app_controller.rb
@@ -0,0 +1,31 @@
+class NoAccountSelectedException < RuntimeError; end
+
+class AppController < ApplicationController
+  include Breadcrumbs
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  http_basic_authenticate_with name: "admin", password: "pwd"
+
+  respond_to :html
+
+  before_action :require_account, except: :index
+  before_action :set_breadcrumbs, except: :index
+
+  private
+
+    def current_account
+      @current_account ||= Account.find(params[:account_id])  if params[:account_id]
+    end
+    helper_method :current_account
+
+    def require_account
+      raise NoAccountSelectedException unless current_account
+    end
+
+    def index
+    end
+
+end
+
