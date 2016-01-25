432d61397af24de8ed39c6aa621e20c39503f7cb
Guilherme Kuhn <g.kuhn0@gmail.com>
2016-01-25 17:51:18 -0200

    Added dependent destroy to tack and messages

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,8 @@
 class Contact < ActiveRecord::Base
   belongs_to :account
 
-  has_many :tracks
-  has_many :messages
+  has_many :tracks, dependent: :destroy
+  has_many :messages, dependent: :destroy
 
   validates :account_id, :identifier, presence: true
   validates :email, uniqueness: {scope: :account_id}, unless: 'email.blank?'
